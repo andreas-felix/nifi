b0cc9176922e262bab26f2be5b45f0b029989774
Andreas Felix <andreas.felix@ebcont.com>
2022-03-16 11:57:24 +0100

    Integrate versions: * nifi 1.11.3 * marklogic java-api 5.5.1 * marklogic client util 4.3.0 * marklogic DMSDK 2.4.0 * remove logging causes log spam

--- nifi-marklogic-processors/src/main/java/org/apache/nifi/marklogic/processor/PutMarkLogic.java
+++ nifi-marklogic-processors/src/main/java/org/apache/nifi/marklogic/processor/PutMarkLogic.java
@@ -342,9 +342,9 @@ public class PutMarkLogic extends AbstractMarkLogicProcessor {
         try {
             FlowFile flowFile = session.get();
             if (flowFile == null) {
-                getLogger().info("Flushing the WriteBatcher asynchronously in case a number of documents less than batchSize are waiting to be written");
+                //getLogger().info("Flushing the WriteBatcher asynchronously in case a number of documents less than batchSize are waiting to be written");
                 flushWriteBatcherAsync(this.writeBatcher);
-                getLogger().info("Calling yield() on the ProcessContext");
+                //getLogger().info("Calling yield() on the ProcessContext");
                 context.yield();
             } else {
                 String duplicateHandler = context.getProperty(DUPLICATE_URI_HANDLING).getValue();
